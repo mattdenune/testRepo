5f38301700c216f57c0364492c8455e1093ca71c
glumac <mglumac@gmail.com>
2018-07-31 14:26:15 -0400

    homework part 1

---
+++ index.js
@@ -0,0 +1,67 @@
+/********************************
+ *
+ * DO YOUR ASSIGNMENT IN THIS FILE
+ *
+ *********************************/
+
+// TEST EARLY AND OFTEN - node starter.js in the terminal
+// After each step, git add . AND git commit -m "completed part PART-LETTER-HERE"
+// Complete all four steps specified below - each step needs a console.log statement.
+// EXAMPLE - Try running node starter.js before working on any of the parts and see what happens in the terminal.
+const givenVariable = 20;
+const doubledVariable = givenVariable * 2;
+const tripledVariable = givenVariable * 3;
+console.log(doubledVariable);
+console.log('EXAMPLE', 'tripledVariable', tripledVariable);
+// After running this and feeling comfortable, feel free to comment the above block of code out (either give each line a // or highlight the block of code and press COMMAND - / )
+
+// DATATYPES
+
+// STEP A: Pythagorean Theorem
+
+const amount1 = 1;
+const amount2 = 3;
+
+// Write an expression here which computes
+// the correct value of a constant "total" based on the values of a and b
+
+// Log your answer (the value of total) to the console
+
+// STEP B: Basic Conversion
+
+const pounds = 155;
+
+// Write an expression here which computes
+// the correct value of a constant "kilograms"
+
+// Log kilograms to the console
+
+// STEP C: Old Modems
+
+const sizeInMB = 25;
+
+// Write an expression here which computes the correct value of the a constant "timeToDownload" for a 56kb (per second) modem
+
+// Log timeToDownloadto the console
+
+// STEP D: Grand Hotel Tremezzo
+
+// Create a variable using "let" (to indicate mutability) grandHotelTremezzoGuests that is equal to an array containing three strings - 'George Clooney' 'Silvio Berlusconi' and 'Isabella Rossellini'. Log grandHotelTremezzoGuests.
+
+// A new guest is checking in! Add "Fabio" to the end of the array. Log grandHotelTremezzoGuests.
+
+// False alarm! Fabio was on a roller coaster and got hit in the face with a pigeon so he won't be able to stay at the hotel remove him from the end of the array.  (We didn't use this method in class you will have to research.)  Log grandHotelTremezzoGuests.
+
+// A new guest is checking in for real! Add 'Mario Batali' to the end of our array. He is wearing a disguise...but orange crocs. Log grandHotelTremezzoGuests.
+
+// Another new guest is checking in for real! Add 'Mario Balotelli' to the end of our array. Log grandHotelTremezzoGuests.
+
+// Create a variable that is equal to the number of guests staying in our hotel by checking the items count of grandHotelTremezzoGuests array.  Log this.
+
+// Create a variable 'grandHotelTremezzoPastGuests' set to an empty Array.
+
+// George Clooney had a scooter accident so has to check out.  Remove him from our array of guests.  Use both the splice method and the push method to add his name to 'grandHotelTremezzoPastGuests' while simultaniously removing him from grandHotelTremezzoGuests.
+
+// The hotel is going to deliver a bottle of Nero de Como to each guest. Use a for loop to log out '*Silvio Berlusconi* gets a bottle of Nero de Como' for each guest.
+
+// The guests drank their bottles of Nero de Como and are getting rowdy. Tell them to be quiet by using a for loop to say ... '*Silvio Berlusconi* please hush up!!!'
---
+++ readme.md
@@ -0,0 +1,36 @@
+# HOMEWORK: JS Datatypes and intro to iteration
+
+![Learn JS](./assets/learn.jpg)
+
+Please read the directions for each part thoroughly.
+
+## 🚀 Before getting started, here's how to submit the homework
+
+1.  Fork this repo to your own account
+2.  Clone it to your local machine from your account using git clone
+3.  Do the homework in the `datatypes/index.js` file
+
+- To test your code, run `node ndex.js`
+
+4.  Push your work up to github with the following steps:
+
+- `git add .`
+- `git commit -m "commit message goes here"`
+- `git push origin master`
+
+## 🚀 Pull request template!!
+
+- On github, open up a pull request
+- Add your name to the title section
+- to copy and paste the [template](https://git.generalassemb.ly/wdi-nyc-peloton/peloton-class-info/blob/master/pull-request-template.md) click the pencil icon (edit this file) then cmd + c
+- cmd + v into the body of your pull request, then edit the information
+
+## 🚀 Completion looks like:
+
+- `index.js` has been filled out in accordance with the instructions.
+
+## 🚀 Your homework submission should include:
+
+- A pull request created on _this repo_.
+
+## This homework is due 🚨 11PM 🚨
