a4455a0439a43c56c5f832f36efcfa5f77f88555
Rachel Moskowitz <rachelbmoskowitz@gmail.com>
2019-08-15 09:06:32 -0400

    refactored lab to be more cohesive and added better instructions

--- index.js
+++ index.js
@@ -4,10 +4,14 @@
  *
  *********************************/
 
-// TEST EARLY AND OFTEN - node index.js in the terminal
+// TEST EARLY AND OFTEN - run the code in the terminal by typing 'node index.js' in this directory
 // After each step, git add . AND git commit -m "completed part PART-LETTER-HERE"
-// Complete all four steps specified below - each step needs a console.log statement.
-// EXAMPLE - Try running node starter.js before working on any of the parts and see what happens in the terminal.
+// Complete all steps specified below - each piece of each step needs to be logged to the console
+// using console.log.
+// EXAMPLE - Try running node index.js before working on any of the parts and see what happens in the terminal.
+
+
+
 const givenVariable = 20;
 const doubledVariable = givenVariable * 2;
 const tripledVariable = givenVariable * 3;
@@ -23,9 +27,19 @@ console.log('EXAMPLE', 'tripledVariable', tripledVariable);
  *
  *********************************/
 
-// Set a constant equal to the name of our SEI instance (capitalized) with the name 'bestClass'.  Log this value.
 
-// Set a bestClassLowercase variable converting bestClass to a lowercase string. Log this value.
+// Set a constant named 'bestClass' equal to the name of our SEI class (Oasis) capitalized.
+// Log this value to the console.
+
+// let bestClass = 'Oasis'
+// let bestClassLowercase = bestClass.toLowerCase()
+
+// Set a constant named bestClassLowercase that converts bestClass to an all lowercase string.
+// Log this value to the console.
+
+
+
+
 
 /********************************
  *
@@ -36,10 +50,15 @@ console.log('EXAMPLE', 'tripledVariable', tripledVariable);
 const amount1 = 1;
 const amount2 = 3;
 
-// Write an expression here which computes
-// the correct value of a constant "total" based on the values of a and b
+// Write an expression here which adds the values of amount1 and amount2 and
+// sets them to a variable called total
+// Log the value of total to the console
 
-// Log your answer (the value of total) to the console
+
+
+
+
+
 
 /********************************
  *
@@ -49,62 +68,220 @@ const amount2 = 3;
 
 const pounds = 155;
 
-// Write an expression here which computes
-// the correct value of a variable "kilograms"
-
+// Write an expression here which converts pounds to kilograms and set the value equal
+// to a variable called "kilograms"
 // Log kilograms to the console
+
+
+
+
+
 
 /********************************
  *
- * STEP D: Grand Hotel Tremezzo
+ * STEP D: Grand Hotel Tremezzo: Part 1
  *
  *********************************/
 
+
+
+
+
 // Create a variable grandHotelTremezzoGuests that is equal to an array containing three strings:
-// 'George Clooney' 'Silvio Berlusconi'  and 'Isabella Rossellini'. Log grandHotelTremezzoGuests.
+// 'George Clooney' 'Silvio Berlusconi'  and 'Isabella Rossellini'.
+// Log grandHotelTremezzoGuests to the console.
 
-// A new guest is checking in! Add "Fabio" to the end of the array. Log grandHotelTremezzoGuests.
 
-// False alarm! Fabio was on a roller coaster and got hit in the face with a pigeon 
-// so he won't be able to stay at the hotel remove him from the end of the array.  
-// (We didn't use this method in class you will have to research.)  Log grandHotelTremezzoGuests.
 
-// A new guest is checking in for real! Add 'Mario Batali' to the end of our array. 
-// He is wearing a disguise...but orange crocs. Log grandHotelTremezzoGuests.
 
-// Another new guest is checking in for real! Add 'Mario Balotelli' to the end of our array. 
-// Log grandHotelTremezzoGuests.
 
-// Create a variable that is equal to the number of guests staying in our hotel by 
-// checking the items count of grandHotelTremezzoGuests array.  Log this.
+
+// A new guest is checking in! Add "Fabio" to the end of the array.
+// Log grandHotelTremezzoGuests to the console.
+
+
+
+
+
+
+
+// False alarm! Fabio was on a roller coaster and got hit in the face with a pigeon
+// so he won't be able to stay at the hotel remove him from the end of the array.
+// Log grandHotelTremezzoGuests to the console.
+
+
+
+
+
+
+
+// A new guest is checking in for real! Add 'Mario Batali' to the end of our array.
+// He is wearing a disguise...but orange crocs.
+// Log grandHotelTremezzoGuests to the console.
+
+
+
+
+
+
+
+
+
+// Another new guest is checking in for real! Add 'Mario Balotelli' to the end of our array.
+// Log grandHotelTremezzoGuests to the console.
+
+
+
+
+
+
+
+
+
+// Create a variable called totalGuests that is equal to the number of guests staying in our hotel
+// by checking the items count of grandHotelTremezzoGuests array.
+// Log totalGuests to the console.
+
+
+
+/********************************
+ *
+ * STEP E: Grand Hotel Tremezzo: Part 2
+ *
+ *********************************/
+
+
+
+
 
 // Create a variable 'grandHotelTremezzoPastGuests' set to an empty Array.
+// Log grandHotelTremezzoPastGuests to the console
 
-// George Clooney had a scooter accident so has to check out.  
-// Remove him from our array of guests.  Use both the splice method 
-// and the push method to add his name to 'grandHotelTremezzoPastGuests' 
-// while simultaneously removing him from grandHotelTremezzoGuests. 
-// Log both grandHotelTremezzoGuests and grandHotelTremezzoPastGuests in the same log statement.
 
-// ***Re-assign*** the variable that is equal to the number of guests staying 
-// in our hotel by checking the items count of grandHotelTremezzoGuests array.  
-// (Don't create a new variable)  Log this.
 
-// The hotel is going to deliver a bottle of Nero de Como to each guest. 
-// Use a for loop to log out '*Silvio Berlusconi* gets a bottle of Nero de Como' for each guest.
 
-// The guests drank their bottles of Nero de Como and are getting rowdy. 
-// Tell them to be quiet by using a for loop to say ... '*Silvio Berlusconi* please hush up!!!'
 
-// Create an empty array sleepingGuests
 
-// The guests passed out. Loop over the guests and add them to the sleepingGuests array.  
-// (Don't remove them from grandHotelTremezzoGuests.) Log out sleepingGuests.
 
-// Lake Como has started to flood so all of the guests are fleeing to Milan. 
-// Use splice to remove all of the guests (simultaneously using push to add them 
-// to the array of checked out guests). Log both grandHotelTremezzoGuests 
+
+// George Clooney had a scooter accident so he has to check out.
+// Remove him from grandHotelTremezzoGuests using the splice method. (Google and MDN are your friends here 😊)
+// Then use the push method to add his name to 'grandHotelTremezzoPastGuests'
+// Log both grandHotelTremezzoGuests and grandHotelTremezzoPastGuests in the same console.log statement.
+
+
+
+
+
+
+
+
+
+
+// ***Re-assign*** the variable that is equal to the number of guests staying
+// in our hotel by checking the currnt items count of grandHotelTremezzoGuests array.
+// (Don't create a new variable!!)
+//  Log the variable to the console.
+
+
+
+
+
+
+
+
+/********************************
+ *
+ * STEP F: Grand Hotel Tremezzo: Part 3
+ *
+ *********************************/
+
+
+
+// The hotel is going to deliver a bottle of Nero de Como to each guest.
+// Use a for loop to log out '*PERSON'S NAME* gets a bottle of Nero de Como'
+// for each guest in the grandHotelTremezzoGuests array.
+
+
+
+
+
+
+
+
+
+// The guests drank their bottles of Nero de Como and are getting rowdy.
+// Tell them to be quiet by using a for loop to log ... '*PERSON'S NAME* please hush up!!!'
+// for each guest in the grandHotelTremezzoGuests array.
+
+
+
+
+
+
+/********************************
+ *
+ * STEP G: Grand Hotel Tremezzo: Part 4
+ *
+ *********************************/
+
+
+// Create an empty array assigned to a variable called sleepingGuests
+// Log sleepingGuests to the console.
+
+
+
+
+
+
+
+// The guests passed out. Loop over the guests and add them to the sleepingGuests array.
+// (Don't remove them from grandHotelTremezzoGuests.)
+// Log sleepingGuests to the console.
+
+
+
+
+
+/********************************
+ *
+ * BONUS!!!!!!!
+ *
+ *********************************/
+
+
+
+// Lake Como has started to flood so all of the guests are fleeing to Milan.
+// Use splice to remove all of the guests (simultaneously using push to add them
+// to the array of checked out guests). Log both grandHotelTremezzoGuests
 // and grandHotelTremezzoPastGuests in the same log statement.
 
-// Create a variable isGrandHotelTremezzoEmpty to be equal to a 
-// boolean with the appropriate value (it is empty). Log this variable.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
